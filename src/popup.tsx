fa25f7d3de58361a0e90a2ad253e6b38fd1c1b3c
lkekana <theoriginalles@gmail.com>
2024-12-30 12:16:47 +0200

    use classless css in popup

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import "../public/shadcn-inspired-classless.css";
 
 const Popup = () => {
   const [count, setCount] = useState(0);
@@ -32,21 +33,73 @@ const Popup = () => {
     });
   };
 
+  // return (
+  //   <>
+  //     <ul style={{ minWidth: "700px" }}>
+  //       <li>Current URL: {currentURL}</li>
+  //       <li>Current Time: {new Date().toLocaleTimeString()}</li>
+  //     </ul>
+  //     <button
+  //       onClick={() => setCount(count + 1)}
+  //       style={{ marginRight: "5px" }}
+  //     >
+  //       count up
+  //     </button>
+  //     <button onClick={changeBackground}>change background</button>
+  //   </>
+  // );
   return (
     <>
-      <ul style={{ minWidth: "700px" }}>
-        <li>Current URL: {currentURL}</li>
-        <li>Current Time: {new Date().toLocaleTimeString()}</li>
-      </ul>
-      <button
-        onClick={() => setCount(count + 1)}
-        style={{ marginRight: "5px" }}
-      >
-        count up
-      </button>
-      <button onClick={changeBackground}>change background</button>
+      {/* Header */}
+      <header>
+        <hgroup>
+          <h1>shadcn-inspired</h1>
+          <p>A class-less example inspired by shadcn components.</p>
+        </hgroup>
+        {/* <button onclick="toggleDarkMode()">Toggle theme</button> */}
+        <button onClick={changeBackground}>Toggle theme</button>
+      </header>
+
+      {/* Main */}
+      <main>
+        {/* Preview */}
+        <section id="preview">
+          <h2>Preview</h2>
+          <p>
+            Sed ultricies dolor non ante vulputate hendrerit. Vivamus sit amet
+            suscipit sapien. Nulla iaculis eros a elit pharetra egestas.
+          </p>
+          <form>
+            <input
+              type="text"
+              name="firstname"
+              placeholder="First name"
+              aria-label="First name"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email address"
+              aria-label="Email address"
+              autoComplete="email"
+              required
+            />
+            <button type="submit">Subscribe</button>
+            <fieldset>
+              <label htmlFor="terms">
+                <input type="checkbox" role="switch" id="terms" name="terms" />
+                I agree to the
+                <a href="#" onClick={(e) => e.preventDefault()}>
+                  Privacy Policy
+                </a>
+              </label>
+            </fieldset>
+          </form>
+        </section>
+      </main>
     </>
-  );
+  )
 };
 
 const root = createRoot(document.getElementById("root")!);
