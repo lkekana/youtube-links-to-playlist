330016ea15252d9a1593634454800f7449cf7ae4
lkekana <theoriginalles@gmail.com>
2025-01-02 13:14:47 +0200

    clean up form

--- src/popup.tsx
+++ src/popup.tsx
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./tailwind.css";
 import { processLinks } from "./youtube";
 
-const INSTRUCTION_TEXT = `Enter your YouTube video link(s) here. 1 video link per line.
-Both video links & IDs are supported.
+const INSTRUCTION_TEXT = `Enter your YouTube video link(s) or ID(s) here. 1 video per line.
 
 You can also paste a playlist link to add all videos in the playlist
 (playlist links must be in the form: https://www.youtube.com/playlist?list={playlist_id})`;
 
-const LINK_BOX_PLACEHOLDER = `Examples:
+const LINK_BOX_PLACEHOLDER = `Enter your YouTube video link(s) or ID(s) here. 1 video per line.
+
+You can also paste a playlist link to add all videos in the playlist
+(playlist links must be in the form: https://www.youtube.com/playlist?list={playlist_id})
+
+Examples:
 https://www.youtube.com/watch?v=pZwvrxVavnQ
 https://www.youtube.com/watch?v=Rk2FR8YflrE&t=1s
 https://youtu.be/3xottY-7m3k
@@ -68,41 +72,73 @@ const Popup = () => {
 			<main className={"min-w-96 w-max"}>
 				{/* Main Form */}
 				<section id="main-form">
-				<h1 className="text-center">▶</h1>
-				<h2 className="text-center">Create a new playlist</h2>
-				<p style={{ whiteSpace: "pre-line" }} className="text-sm">
+					<h1 className="text-center">▶</h1>
+					<h2 className="text-center">Create a new playlist</h2>
+
+					{/* Text */}
+					<input
+						type="text"
+						id="title"
+						name="title"
+						placeholder="Choose a playlist title"
+						required
+						className="w-full text-sm resize-none m-1"
+					/>
+
+					{/* Select */}
+					<select
+						id="privacy-select"
+						name="privacy-select"
+						required
+						value={privacy}
+						onChange={handlePrivacyChange}
+						className="w-full m-1"
+					>
+						<option value={PlaylistPrivacy.PUBLIC}>
+						🌎 Public: Anyone can search for and view
+						</option>
+						<option value={PlaylistPrivacy.UNLISTED}>
+						🔗 Unlisted: Anyone with the link can view
+						</option>
+						<option value={PlaylistPrivacy.PRIVATE}>
+						🔒 Private: Only you can view
+						</option>
+					</select>
+
+					{/* Instructions */}
+					{/* <p style={{ whiteSpace: "pre-line" }} className="text-sm">
 						{INSTRUCTION_TEXT}
-					</p>
-					<form>
+					</p> */}
+					<form className="m-1">
 						{/* Link Box */}
 						<textarea
 							id="text"
 							name="text"
 							placeholder={LINK_BOX_PLACEHOLDER}
 							rows={10} // Adjust number of visible rows
-							className="w-full font-mono text-sm resize-none"
+							className="w-full font-mono text-xs resize-none"
 							readOnly={processing}
 							aria-busy={processing}
 							value={text}
-                            onChange={handleChange}
-                        />
+							onChange={handleChange}
+						/>
 
 						{processing ? (
-						  <button 
-							type="reset" 
-							style={{ backgroundColor: "var(--muted-foreground)" }}
-							onClick={cancelClick}
-						  >
-							Cancel
-						  </button>
+							<button
+								type="reset"
+								style={{ backgroundColor: "var(--muted-foreground)" }}
+								onClick={cancelClick}
+							>
+								Cancel
+							</button>
 						) : (
-						  <button 
-							type="submit" 
-							style={{ backgroundColor: "var(--primary)" }}
-							onClick={submitClick}
-						  >
-							Create
-						  </button>
+							<button
+								type="submit"
+								style={{ backgroundColor: "var(--primary)" }}
+								onClick={submitClick}
+							>
+								Create
+							</button>
 						)}
 					</form>
 				</section>
